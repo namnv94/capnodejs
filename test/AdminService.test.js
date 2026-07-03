6f7b3869c320b904563b1fbd86acff6ca4abd8da
Nguyen Van Nam (BD/PTD-TAF15) <gan9hc@bosch.com>
2026-07-03 14:22:35 +0700

    add cds test

---
+++ test/AdminService.test.js
@@ -0,0 +1,14 @@
+const cds = require('@sap/cds')
+
+const { GET, POST, expect, defaults } = cds.test (__dirname+'/..')
+
+describe('AdminService OData APIs', () => {
+
+  it('serves AdminService.Books', async () => {
+    const { data } = await GET `/odata/v4/admin/Books ${{ params: { $select: 'ID,title' } }}`
+    expect(data.value).to.containSubset([
+      {"ID":25415501,"title":"title-25415501"},
+    ])
+  })
+
+})
