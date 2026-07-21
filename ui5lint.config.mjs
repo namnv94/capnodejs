23631cffa3021224ddedbd8cc8930e197e75aa95
Nguyen Van Nam (BD/PTD-TAF15) <gan9hc@bosch.com>
2026-07-22 00:17:49 +0700

    update ui5lint

--- ui5lint.config.mjs
+++ ui5lint.config.mjs
@@ -1,34 +1,32 @@
 export default {
-  lint: {
-    extends: ['recommended'],
+  extends: ['recommended'],
 
-    rules: {
-      /*
-       * XML Views
-       */
-      'xml-unused-namespaces': 'warn',
-      'xml-duplicate-ids': 'error',
+  rules: {
+    /*
+     * XML Views
+     */
+    'xml-unused-namespaces': 'warn',
+    'xml-duplicate-ids': 'error',
 
-      /*
-       * JavaScript
-       */
-      'no-hardcoded-urls': 'warn',
-      'no-global-functions': 'warn',
+    /*
+     * JavaScript
+     */
+    'no-hardcoded-urls': 'warn',
+    'no-global-functions': 'warn',
 
-      /*
-       * Manifest
-       */
-      'manifest-sapui5-dependencies': 'warn',
+    /*
+     * Manifest
+     */
+    'manifest-sapui5-dependencies': 'warn',
 
-      /*
-       * Internationalization
-       */
-      'no-hardcoded-text': 'warn',
+    /*
+     * Internationalization
+     */
+    'no-hardcoded-text': 'warn',
 
-      /*
-       * General
-       */
-      'no-sync-ajax': 'error'
-    }
+    /*
+     * General
+     */
+    'no-sync-ajax': 'error'
   }
 };
